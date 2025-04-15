4040d420d2e2c14d68cdba4b7dbc53d9c49f1ca7
Maxwell Norman <KoopaPig@users.noreply.github.com>
2025-04-15 03:53:58 -0400

    Fixed figure sizing
    - Made new prefabs for in-capsule and in-collection
    - Made all bases a consistent size
    - Figures are now instantiated with worldspace size

--- Assets/Scripts/Player/PlayerAttachedFigure.cs
+++ Assets/Scripts/Player/PlayerAttachedFigure.cs
@@ -8,7 +8,24 @@ namespace GASHAPWN {
 
         public void SetFigureInCapsule(Figure figure) {
             attachedFigure = figure;
-            Instantiate(figure.capsuleModelPrefab, capsPos.parent.transform);
+
+            // All of this below ensures the scale of the model matches the world scale
+
+            var obj = Instantiate(figure.capsuleModelPrefab);
+
+            Vector3 originalScale = obj.transform.lossyScale;
+            obj.transform.SetParent(capsPos, false);
+            obj.transform.localPosition = Vector3.zero;
+            obj.transform.localRotation = Quaternion.identity;
+
+            // Restore world scale by recalculating local scale relative to parent
+            Transform parent = obj.transform.parent;
+            Vector3 parentScale = parent.lossyScale;
+            obj.transform.localScale = new Vector3(
+                originalScale.x / parentScale.x,
+                originalScale.y / parentScale.y,
+                originalScale.z / parentScale.z
+            );
         }
 
         public Figure GetAttachedFigure() { return attachedFigure; }
